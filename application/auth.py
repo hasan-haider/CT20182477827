783ecd8f23841aa877905bcb315ecc529c3af000
MrunmayiK <64848702+MrunmayiK@users.noreply.github.com>
2020-06-13 21:35:19 +0530

    Update auth.py
    (Mrunmayi) 
    This file is for login and signup page

--- application/auth.py
+++ application/auth.py
@@ -5,12 +5,12 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@auth.route('/logout')
+'''@auth.route('/logout')
 def logout():
-    return 'logout'
+    return 'logout' '''
