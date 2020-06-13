c5c13ab71ab243f8d27afd29c12521753cb6f087
nileshpandey31 <nileshpandey786786@gmail.com>
2020-06-13 22:33:58 +0530

    state-city updated
    modified the custom.js page for state-city
    
    also made cganges in insert.html to support custom.js

--- application/static/js/custom.js
+++ application/static/js/custom.js
@@ -1,3 +1,17 @@
+//staes n city in india
 
-var state = Object();
-city_states['India'] = '|New Delhi||Andaman/Nicobar Islands|Andhra Pradesh|Arunachal Pradesh|Assam|Bihar|Chandigarh|Chhattisgarh|Dadra/Nagar Haveli|Daman/Diu|Goa|Gujarat|Haryana|Himachal Pradesh|Jammu/Kashmir|Jharkhand|Karnataka|Kerala|Lakshadweep|Madhya Pradesh|Maharashtra|Manipur|Meghalaya|Mizoram|Nagaland|Orissa|Pondicherry|Punjab|Rajasthan|Sikkim|Tamil Nadu|Tripura|Uttaranchal|Uttar Pradesh|West Bengal';
+var citiesByState = {
+    Odisha: ["Bhubaneswar","Puri","Cuttack"],
+    Maharashtra: ["Mumbai","Pune","Nagpur","boisar"],
+    Kerala: ["kochi","Kanpur",'lucknow']
+    }
+    function makeSubmenu(value) {
+    if(value.length==0) document.getElementById("citySelect").innerHTML = "<option></option>";
+    else {
+    var citiesOptions = "";
+    for(cityId in citiesByState[value]) {
+    citiesOptions+="<option>"+citiesByState[value][cityId]+"</option>";
+    }
+    document.getElementById("citySelect").innerHTML = citiesOptions;
+    }
+    }
