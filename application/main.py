078d852807e6195900ee604b5f2e05382b430d82
MrunmayiK <64848702+MrunmayiK@users.noreply.github.com>
2020-06-13 21:30:10 +0530

    Update main.py

--- application/main.py
+++ application/main.py
@@ -3,10 +3,11 @@ from . import db
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/index')
 def index():
     return render_template('index.html')
 
-@main.route('/profile')
+#html page for profile or first page
+'''@main.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html')'''
